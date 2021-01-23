6940622795ac4a06414a06d9bf5b8e09382c5cd5
prabhath-amarasinghe <prabhath.asinghe@gmail.com>
2021-01-23 15:51:53 +0000

    Initial commit

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+version=$(<version.txt)
+volume=$(<volume.txt)
+target=$(<target.txt)
+
+
+if [[ -z $(docker images -q mongo:$version) ]]; then
+   echo "No Mongo:$version image found. Pulling from docker hub"
+   docker pull mongo:$version
+   
+else
+    echo "Found already existing image mongo:$version"
+   
+fi
+sudo docker run -d -p 27017:27017 -v $volume:$target mongo:$version
